be3ebd83d7d588c0147226a4318122eff7d70f61
milenagrutka <milenagbelo@gmail.com>
2024-08-22 16:12:08 -0300

    Update index.js

--- aula 7/index.js
+++ aula 7/index.js
@@ -58,43 +58,82 @@ console.log("Idade em 2050",idade+idadeEm2050)
 //Exercícios de fixação de interpretação de código:
 //Exercício1
 const bool1 = true
+//bool1 recebe verdadeiro
 const bool2 = false
+//bool2 recebe falso
 const bool3 = !bool2
+//bool3 recebe o inverso de bool2
 
 let resultado = bool1 && bool2
 console.log("a. ", resultado)
-//
+//imprime bool1 && bool2 falso
 resultado = bool1 && bool2 && bool3
 console.log("b. ", resultado)
-//
+//Imprime bool1 && bool2 && bool3 falso
 resultado = !resultado && (bool1 || bool2)
 console.log("c. ", resultado)
-//
+//Imprime o resultado anterior invertido && (bool1 || bool2) true
 console.log("d. ", typeof resultado)
-//
+//Imprime o tipo do resultado anterior bool
 */
 
-
+/*
 //Exercício2
 let primeiroNumero = prompt("Digite um número!")
 let segundoNumero = prompt("Digite outro número!")
-
 const soma = primeiroNumero + segundoNumero
-
 console.log(soma)
-//
+//Será impresso a junção do primeiro número com o segundo. Para ajustar devemos converter para Number os prompts.
 
-//Exercício de ficação de escrita de código:
+//Solução para os códigos impressos:
+let primeiroNumero = Number (prompt("Digite um número!"))
+let segundoNumero = Number (prompt("Digite outro número!"))
+const soma = primeiroNumero + segundoNumero
+console.log(soma)
+*/
+
+/*
+//Exercício de fixação de escrita de código:
 //Exercício1
-const idade = prompt("qual sua idade?")
+const idade = Number (prompt("Qual sua idade?"))
 console.log("idade:", idade)
 
-const idadeAmigo = prompt("qual a idade do seu melhor amigo?")
+const idadeAmigo = Number (prompt("Qual a idade do seu melhor amigo(a)?"))
 console.log("idadeAmigo:", idadeAmigo)
 
-console.log("sua idade é maior do que a do seu melhor amigo?", true ou false)
-//
+let resultado = idade > idadeAmigo
+console.log("Sua idade é maior do que a do seu melhor amigo?", resultado)
+*/
 
+/*
 //Exercício2
-const numeroPar = prompt("Insira um número par")
-//
+//a-
+const numeroPar = Number(prompt("Insira um número par"))
+//b-
+console.log(numeroPar%2)
+//c- Sim, sempre dá o número 0 pois um número par sempre é divido por 2.
+//d- O resto da divisão é diferente de 0.
+*/
+
+/*
+//Exercício3
+const idade = Number(prompt("Qual sua idade?"))
+//a-
+const resultado = idade * 12
+console.log("Sua idade em meses é:", resultado)
+//b-
+let resultado1 = idade * 365
+console.log("Sua idade em dias é:", resultado1)
+//c-
+resultado1 *= 24
+console.log("Sua idade em horas é:", resultado1)
+*/
+
+//Exercício4
+const numeroUm = Number(prompt("Insira um número:"))
+const numeroDois = Number(prompt("Insira outro número:"))
+
+const resultado = numeroUm > numeroDois
+const resultado = numeroUm === numeroDois
+const resultado = numeroUm % numeroDois
+const resultado = numeroDois % numeroUm
