6795d1cb2d0d62d554ae8f90a9cd90afad07dec4
milenagrutka <milenagbelo@gmail.com>
2024-08-22 16:14:19 -0300

    Add files via upload

---
+++ aula 8/index.js
@@ -0,0 +1,68 @@
+/*
+const texto1 = "Olá mundo"
+const texto2 = 'Olá mundo'
+const texto3 = `Olá mundo`
+
+const nome = "Milena"
+const idade = 26
+*/
+
+/*
+//Exemplo concatenação:
+const frase = "O meu nome é: " + nome + " e minha idade é: " + idade + " anos."
+console.log(frase)
+*/
+
+/*
+//Exemplo template strings:
+const frase = `Meu nome é: ${nome} e minha idade é: ${idade} anos`
+console.log(frase)
+*/
+
+/*
+//Exercício1
+const nomeDoUser = Milena
+const corFavorita = Roxo
+//template string:
+const frase = `Nome: ${nomeDoUser} e Sua cor favorita é: ${corFavorita}`
+console.log(frase)
+//contatenação:
+const frase = "Nome: " + nomeDoUser + " Sua cor favorita? " + corFavorita
+console.log(frase)
+*/
+
+/*
+//Protótipo de Strings
+//Propriedade length
+const nome = `Milena`
+console.log(nome.length)
+//toLowerCase()
+console.log(nome.toLowerCase())
+//toUpperCase()
+console.log(nome.toUpperCase())
+
+const email = " milenagbelo@gmail.com "
+//trim()
+console.log(email.trim())
+*/
+
+/*
+const frase = "Hoje comi cenoura"
+console.log(frase.includes("cenoura"))
+
+console.log(frase.incluides("batata"))
+*/
+
+/*
+//replaceAll
+const frase = "Hoje comi cenoura, adoro cenoura."
+const novaFrase = frase.replaceAll("cenoura","batata")
+
+console.log(novaFrase)
+*/
+
+//Exercício
+const frase = "EU AMO O BTS"
+const novaFrase = frase.replaceAll("EU AMO O BTS","EU AMI I BTS")
+console.log(novaFrase)
+console.log(frase.length)
